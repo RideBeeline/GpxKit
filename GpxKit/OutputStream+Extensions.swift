b7ed8763e57da4ff9656b1d3343784dc89e03d09
Chetan Padia <chetbox@users.noreply.github.com>
2017-10-26 15:32:22 +0100

    move string extension to private extension method

--- GpxKit/OutputStream+Extensions.swift
+++ GpxKit/OutputStream+Extensions.swift
@@ -41,3 +41,14 @@ extension OutputStream {
     }
 
 }
+
+private extension String {
+
+    func xmlEscape() -> String {
+        return replacingOccurrences(of: "&", with: "&amp;")
+            .replacingOccurrences(of: "\"", with: "&quot;")
+            .replacingOccurrences(of: ">", with: "&gt;")
+            .replacingOccurrences(of: "<", with: "&lt;")
+    }
+
+}
