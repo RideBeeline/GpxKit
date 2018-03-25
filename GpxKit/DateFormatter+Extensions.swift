d6398b1fe32f715bbbb8bbf74831c357bfeab304
Tomer Shalom <tomer.shalom@gmail.com>
2018-03-25 19:40:55 +0300

    fix time formatter timezone issue

--- GpxKit/DateFormatter+Extensions.swift
+++ GpxKit/DateFormatter+Extensions.swift
@@ -11,7 +11,7 @@ extension DateFormatter {
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
         return formatter
     }()
 }
